c8080ba04d64e43fa558998c9b50c19c7dc05cd5
Damian Lasecki <damian.x.lasecki@gsk.com>
2020-01-22 19:02:25 +0100

    Perm unrank works

--- main.py
+++ main.py
@@ -77,10 +77,10 @@ if __name__ == '__main__':
 
     def permRank(T, n):
         r  = 0
-        for j in range(2, n+1, 1):
+        for j in range(1, n, 1):
             
-            k = 1
-            i = 1 
+            k = 0
+            i = 0 
             while T[i] != j:
                 
                 if T[i] < j:
