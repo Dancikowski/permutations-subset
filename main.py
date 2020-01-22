757758922b175681d7b222d4c1ab7b13c8ca73b4
Damian Lasecki <damian.x.lasecki@gsk.com>
2020-01-22 19:02:05 +0100

    Perm unrank works

--- main.py
+++ main.py
@@ -15,6 +15,21 @@ class PermutationSetup(object):
         else:
             return n/ k * self.newton(n - 1, k - 1)
 
+    def permRank(self, T, n):
+        r  = 0
+        for j in range(2, n-1, 1):
+            k = 1
+            i = 1 
+            while T[i] != j:
+                if T[i] < j:
+                    k = k + 1
+                i = i + 1
+            if r % 2 == 0:
+                r = j * r + j - k
+            else: 
+                r = j * r + k - 1     
+
+
     def kSubsetRevDoorRank(self, T, k):
         r = - (k % 2)
         s = 1
@@ -47,16 +62,34 @@ class PermutationSetup(object):
 
 if __name__ == '__main__':
 
-    vertex = list(map(int, input("Please type vertex as: num1, num2, num3, numx: ").split(',')))
-    k = int(input("Please type lenght of vertex: "))
-    n = int(input("Please type range of permutation: "))
-    nextPos = int(input("Choose if you want to get successor or predecessor. Type 1 to get successor and -1 to get predecessor, respectively:  "))
+    # vertex = list(map(int, input("Please type vertex as: num1, num2, num3, numx: ").split(',')))
+    # k = int(input("Please type lenght of vertex: "))
+    # n = int(input("Please type range of permutation: "))
+    # nextPos = int(input("Choose if you want to get successor or predecessor. Type 1 to get successor and -1 to get predecessor, respectively:  "))
   
-    if vertex and k and n and nextPos:
-        permutationSetup = PermutationSetup(vertex, k, n, nextPos)
-        print(permutationSetup.getSuccessorOrPredecessor())
-    else:
-        print("Bad arguments")
+    # if vertex and k and n and nextPos:
+    #     permutationSetup = PermutationSetup(vertex, k, n, nextPos)
+    #     print(permutationSetup.getSuccessorOrPredecessor())
+    # else:
+    #     print("Bad arguments")
 
 
 
+    def permRank(T, n):
+        r  = 0
+        for j in range(2, n+1, 1):
+            
+            k = 1
+            i = 1 
+            while T[i] != j:
+                
+                if T[i] < j:
+                    k = k + 1
+                i = i + 1
+            if r % 2 == 0:
+                r = j * r + j - k
+            else: 
+                r = j * r + k - 1
+        return r 
+
+    print(permRank([None,3,4,2,1],4))
